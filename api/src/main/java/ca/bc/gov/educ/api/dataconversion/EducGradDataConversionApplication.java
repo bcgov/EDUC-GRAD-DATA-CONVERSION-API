9424adaccde2fb302784b8d3e01085eacafeafc9
Kamal <kamal.mohammed@outlook.com>
2021-09-27 16:41:02 -0700

    Publish swagger and actuator endpoints

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/EducGradDataConversionApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/EducGradDataConversionApplication.java
@@ -8,7 +8,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @SpringBootApplication
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -29,5 +33,24 @@ public class EducGradDataConversionApplication {
         modelMapper.typeMap(GradCourseRestriction.class, CourseRestrictionEntity.class);
 
         return modelMapper;
+    }
+
+    @Configuration
+    static
+    class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        /**
+         * Instantiates a new Web security configuration.
+         * This makes sure that security context is propagated to async threads as well.
+         */
+        public WebSecurityConfiguration() {
+            super();
+            SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+        }
+        @Override
+        public void configure(WebSecurity web) {
+            web.ignoring().antMatchers("/api/v1/api-docs-ui.html",
+                    "/api/v1/swagger-ui/**", "/api/v1/api-docs/**",
+                    "/actuator/health","/actuator/prometheus", "/health");
+        }
     }
 }
