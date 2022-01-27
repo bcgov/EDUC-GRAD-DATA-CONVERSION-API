a93d788d2a9409f65e9251b1a15e3077fc32b4cb
Kamal <kamal.mohammed@outlook.com>
2022-01-27 14:49:47 -0700

    CI/CD Updates

--- tools/jenkins/DeployToTest.groovy
+++ tools/jenkins/DeployToTest.groovy
@@ -9,11 +9,17 @@ pipeline{
         APP_SUBDOMAIN_SUFFIX = '77c02f-test'
         APP_DOMAIN = 'apps.silver.devops.gov.bc.ca'
         TAG = 'test'
-        REPO_NAME = 'educ-grad-data-conversion-api'
+        REPO_NAME = 'educ-grad-algorithm-api'
         ORG = 'bcgov'
         BRANCH = 'main'
         SOURCE_REPO_URL = 'https://github.com/${ORG}/${REPO_NAME}'
         SOURCE_REPO_URL_RAW = 'https://raw.githubusercontent.com/${ORG}/${REPO_NAME}'
+    }
+    options {
+        buildDiscarder(logRotator(daysToKeepStr: '', numToKeepStr: '5'))
+    }
+    parameters {
+        choice( name: 'IMAGE_TAG', choices: ['main', 'release/1.0.0', 'dev' ] )
     }
     stages{
         stage('Deploy to TEST') {
@@ -23,7 +29,8 @@ pipeline{
                         openshift.withProject(OCP_PROJECT) {
                             openshift.apply(
                                     openshift.process("-f", "${SOURCE_REPO_URL_RAW}/${BRANCH}/tools/openshift/api.dc.yaml",
-                                            "REPO_NAME=${REPO_NAME}", "HOST_ROUTE=${REPO_NAME}-${APP_SUBDOMAIN_SUFFIX}.${APP_DOMAIN}")
+                                            "REPO_NAME=${REPO_NAME}", "HOST_ROUTE=${REPO_NAME}-${APP_SUBDOMAIN_SUFFIX}.${APP_DOMAIN}",
+                                            "TAG_NAME=${params.IMAGE_TAG}")
                             )
                             openshift.selector("dc", "${REPO_NAME}-dc").rollout().latest()
                             timeout (time: 10, unit: 'MINUTES') {
