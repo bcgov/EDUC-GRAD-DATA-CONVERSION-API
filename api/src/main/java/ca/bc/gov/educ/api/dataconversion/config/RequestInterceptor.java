3244706e86dca235532d19e7cd53938f2c28c219
Jinil Sung <infstar@gmail.com>
2022-05-17 10:06:27 -0700

    Update RequestInterceptor.java
    username from JWT is updated to use preferred_username instead of email.

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/config/RequestInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/config/RequestInterceptor.java
@@ -38,9 +38,9 @@ public class RequestInterceptor implements AsyncHandlerInterceptor {
 		// username
 		JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
 		Jwt jwt = (Jwt) authenticationToken.getCredentials();
-		String email = (String) jwt.getClaims().get("email");
-		if (email != null) {
-			ThreadLocalStateUtil.setCurrentUser(email);
+		String username = (String) jwt.getClaims().get("preferred_username");
+		if (username != null) {
+			ThreadLocalStateUtil.setCurrentUser(username);
 		}
 		return true;
 	}
