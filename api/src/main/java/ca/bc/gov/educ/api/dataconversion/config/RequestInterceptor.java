90bcfada7506889791cb1dbe3528c9d4cf58f243
Jinil Sung <infstar@gmail.com>
2022-05-20 11:58:11 -0700

    Update RequestInterceptor.java
    Enhanced the JWT parsing for username to avoid unexpected mistype cast error.

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/config/RequestInterceptor.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/config/RequestInterceptor.java
@@ -7,6 +7,7 @@ import ca.bc.gov.educ.api.dataconversion.util.ThreadLocalStateUtil;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -37,12 +38,16 @@ public class RequestInterceptor implements AsyncHandlerInterceptor {
 		}
 
 		// username
-		JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-		Jwt jwt = (Jwt) authenticationToken.getCredentials();
-		String username = JwtUtil.getName(jwt);
-		if (username != null) {
-			ThreadLocalStateUtil.setCurrentUser(username);
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth instanceof JwtAuthenticationToken) {
+			JwtAuthenticationToken authenticationToken = (JwtAuthenticationToken) auth;
+			Jwt jwt = (Jwt) authenticationToken.getCredentials();
+			String username = JwtUtil.getName(jwt);
+			if (username != null) {
+				ThreadLocalStateUtil.setCurrentUser(username);
+			}
 		}
+
 		return true;
 	}
 
