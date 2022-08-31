4913bc4998bd59c605c58aa8fddf56cc4187159e
Jinil Sung <infstar@gmail.com>
2022-08-30 19:23:56 -0700

    More coverage work.
    More coverage work.

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/service/student/StudentService.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/service/student/StudentService.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.nio.ByteBuffer;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
--- api/src/test/java/ca/bc/gov/educ/api/dataconversion/service/OngoingUpdateServiceTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/dataconversion/service/OngoingUpdateServiceTest.java
@@ -6,6 +6,7 @@ import ca.bc.gov.educ.api.dataconversion.entity.conv.Event;
 import ca.bc.gov.educ.api.dataconversion.messaging.NatsConnection;
 import ca.bc.gov.educ.api.dataconversion.messaging.jetstream.Subscriber;
 import ca.bc.gov.educ.api.dataconversion.model.*;
+import ca.bc.gov.educ.api.dataconversion.repository.conv.EventRepository;
 import ca.bc.gov.educ.api.dataconversion.service.student.OngoingUpdateService;
 import ca.bc.gov.educ.api.dataconversion.service.student.StudentService;
 import ca.bc.gov.educ.api.dataconversion.util.EducGradDataConversionApiConstants;
@@ -18,9 +19,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -32,6 +36,9 @@ public class OngoingUpdateServiceTest {
 
     @Autowired
     OngoingUpdateService ongoingUpdateService;
+
+    @MockBean
+    EventRepository eventRepository;
 
     @MockBean
     StudentService studentService;
@@ -115,8 +122,12 @@ public class OngoingUpdateServiceTest {
         res.setAccess_token("accessToken");
         res.setRefresh_token("refreshToken");
         when(this.restUtils.getTokenResponseObject()).thenReturn(res);
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -156,8 +167,12 @@ public class OngoingUpdateServiceTest {
         summary.setAccessToken("123");
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -224,8 +239,12 @@ public class OngoingUpdateServiceTest {
         summary.setAccessToken("123");
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -293,12 +312,16 @@ public class OngoingUpdateServiceTest {
         summary.setAccessToken("123");
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
-    public void testProcessStudentForGrad2018ENProgram_givenUpdated_XPROGRAM_whenNewCourseIsAdded_then_returnsAPICallSuccess() {
+    public void testProcessStudentForGrad2018ENProgram_givenUpdated_XPROGRAM_whenCourseIsSame_then_returnsAPICallSuccess() {
         // ID
         UUID studentID = UUID.randomUUID();
         String pen = "111222333";
@@ -353,13 +376,237 @@ public class OngoingUpdateServiceTest {
                 .studentStatus("A")
                 .schoolOfRecord(mincode)
                 .graduationRequestYear("2018")
-                .programCodes(Arrays.asList("XC","AD")).build();
+                .programCodes(Arrays.asList("XC","FI")).build();
         ConversionStudentSummaryDTO summary = new ConversionStudentSummaryDTO();
         summary.setAccessToken("123");
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
+    }
+
+    @Test
+    public void testProcessStudentForGrad2018ENProgram_givenUpdated_XPROGRAM_whenNewCareerProgramIsRemoved_then_returnsAPICallSuccess() {
+        // ID
+        UUID studentID = UUID.randomUUID();
+        String pen = "111222333";
+
+        // Program & School
+        String program = "2018-EN";
+        String mincode = "222333";
+
+        String updateType = "XPROGRAM";
+
+        // TraxUpdateInGrad
+        TraxUpdateInGrad traxUpdateInGrad = new TraxUpdateInGrad();
+        traxUpdateInGrad.setPen(pen);
+        traxUpdateInGrad.setUpdateType(updateType);
+
+        // Event
+        Event event = new Event();
+        event.setEventType(EventType.UPDATE_TRAX_STUDENT_MASTER.name());
+        event.setEventStatus(EventStatus.DB_COMMITTED.name());
+        event.setActivityCode(updateType);
+        event.setEventId(UUID.randomUUID());
+
+        // current GRAD Student
+        StudentGradDTO currentStudent = new StudentGradDTO();
+        // GraduationStudentRecord
+        currentStudent.setStudentID(studentID);
+        currentStudent.setProgram(program);
+        currentStudent.setStudentGrade("12");
+        currentStudent.setStudentStatus("A");
+        currentStudent.setSchoolOfRecord(mincode);
+        currentStudent.setSchoolAtGrad(mincode);
+        // Optional Program Codes
+        currentStudent.getProgramCodes().add("XC");
+
+        // Courses
+        StudentCourse course1 = new StudentCourse();
+        course1.setCourseCode("Test");
+        course1.setCourseLevel("12");
+        course1.setCourseName("Test Course");
+        course1.setCreditsUsedForGrad(Integer.valueOf("4"));
+        course1.setCompletedCoursePercentage(Double.valueOf("92.00"));
+        course1.setCredits(Integer.valueOf("4"));
+        course1.setPen(pen);
+        currentStudent.getCourses().add(course1);
+
+        when(this.studentService.loadStudentData(eq(pen), any())).thenReturn(currentStudent);
+
+        when(this.restUtils.getOptionalProgram(eq(program), eq("XC"), any())).thenReturn(null);
+
+        // ConvGradStudent = traxStudent with the recent updated info
+        ConvGradStudent traxStudent = ConvGradStudent.builder().pen(pen)
+                .program(program)
+                .studentGrade("12")
+                .studentStatus("A")
+                .schoolOfRecord(mincode)
+                .graduationRequestYear("2018")
+                .programCodes(new ArrayList<>()).build();
+        ConversionStudentSummaryDTO summary = new ConversionStudentSummaryDTO();
+        summary.setAccessToken("123");
+
+        when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
+
+        ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
+    }
+
+    @Test
+    public void testProcessStudentForGrad2018ENProgram_givenUpdated_XPROGRAM_whenNewCareerProgramIsAdded_then_returnsAPICallSuccess() {
+        // ID
+        UUID studentID = UUID.randomUUID();
+        String pen = "111222333";
+
+        // Program & School
+        String program = "2018-EN";
+        String mincode = "222333";
+
+        String updateType = "XPROGRAM";
+
+        // TraxUpdateInGrad
+        TraxUpdateInGrad traxUpdateInGrad = new TraxUpdateInGrad();
+        traxUpdateInGrad.setPen(pen);
+        traxUpdateInGrad.setUpdateType(updateType);
+
+        // Event
+        Event event = new Event();
+        event.setEventType(EventType.UPDATE_TRAX_STUDENT_MASTER.name());
+        event.setEventStatus(EventStatus.DB_COMMITTED.name());
+        event.setActivityCode(updateType);
+        event.setEventId(UUID.randomUUID());
+
+        // current GRAD Student
+        StudentGradDTO currentStudent = new StudentGradDTO();
+        // GraduationStudentRecord
+        currentStudent.setStudentID(studentID);
+        currentStudent.setProgram(program);
+        currentStudent.setStudentGrade("12");
+        currentStudent.setStudentStatus("A");
+        currentStudent.setSchoolOfRecord(mincode);
+        currentStudent.setSchoolAtGrad(mincode);
+        // Optional Program Codes
+        // None
+
+        // Courses
+        StudentCourse course1 = new StudentCourse();
+        course1.setCourseCode("Test");
+        course1.setCourseLevel("12");
+        course1.setCourseName("Test Course");
+        course1.setCreditsUsedForGrad(Integer.valueOf("4"));
+        course1.setCompletedCoursePercentage(Double.valueOf("92.00"));
+        course1.setCredits(Integer.valueOf("4"));
+        course1.setPen(pen);
+        currentStudent.getCourses().add(course1);
+
+        when(this.studentService.loadStudentData(eq(pen), any())).thenReturn(currentStudent);
+
+        when(this.restUtils.getOptionalProgram(eq(program), eq("XC"), any())).thenReturn(null);
+
+        // ConvGradStudent = traxStudent with the recent updated info
+        ConvGradStudent traxStudent = ConvGradStudent.builder().pen(pen)
+                .program(program)
+                .studentGrade("12")
+                .studentStatus("A")
+                .schoolOfRecord(mincode)
+                .graduationRequestYear("2018")
+                .programCodes(Arrays.asList("XC")).build();
+        ConversionStudentSummaryDTO summary = new ConversionStudentSummaryDTO();
+        summary.setAccessToken("123");
+
+        when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
+
+        ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
+    }
+
+    @Test
+    public void testProcessStudentForGrad2018ENProgram_givenUpdated_XPROGRAM_whenCourseIsRemoved_then_returnsAPICallSuccess() {
+        // ID
+        UUID studentID = UUID.randomUUID();
+        String pen = "111222333";
+
+        // Program & School
+        String program = "2018-EN";
+        String mincode = "222333";
+
+        String updateType = "XPROGRAM";
+
+        // TraxUpdateInGrad
+        TraxUpdateInGrad traxUpdateInGrad = new TraxUpdateInGrad();
+        traxUpdateInGrad.setPen(pen);
+        traxUpdateInGrad.setUpdateType(updateType);
+
+        // Event
+        Event event = new Event();
+        event.setEventType(EventType.UPDATE_TRAX_STUDENT_MASTER.name());
+        event.setEventStatus(EventStatus.DB_COMMITTED.name());
+        event.setActivityCode(updateType);
+        event.setEventId(UUID.randomUUID());
+
+        // current GRAD Student
+        StudentGradDTO currentStudent = new StudentGradDTO();
+        // GraduationStudentRecord
+        currentStudent.setStudentID(studentID);
+        currentStudent.setProgram(program);
+        currentStudent.setStudentGrade("12");
+        currentStudent.setStudentStatus("A");
+        currentStudent.setSchoolOfRecord(mincode);
+        currentStudent.setSchoolAtGrad(mincode);
+        // Optional Program Codes
+        currentStudent.getProgramCodes().add("ID");
+
+        // Courses
+        StudentCourse course1 = new StudentCourse();
+        course1.setCourseCode("Test");
+        course1.setCourseLevel("12");
+        course1.setCourseName("Test Course");
+        course1.setCreditsUsedForGrad(Integer.valueOf("4"));
+        course1.setCompletedCoursePercentage(Double.valueOf("92.00"));
+        course1.setCredits(Integer.valueOf("4"));
+        course1.setPen(pen);
+        currentStudent.getCourses().add(course1);
+
+        when(this.studentService.loadStudentData(eq(pen), any())).thenReturn(currentStudent);
+
+        when(this.restUtils.getOptionalProgram(eq(program), eq("XC"), any())).thenReturn(null);
+
+        OptionalProgram optionalProgram = new OptionalProgram();
+        optionalProgram.setOptionalProgramID(UUID.randomUUID());
+        optionalProgram.setOptProgramCode("ID");
+        optionalProgram.setOptionalProgramName("IB Diploma");
+        optionalProgram.setGraduationProgramCode(program);
+        when(this.restUtils.getOptionalProgram(eq(program), eq("ID"), any())).thenReturn(optionalProgram);
+
+        // ConvGradStudent = traxStudent with the recent updated info
+        ConvGradStudent traxStudent = ConvGradStudent.builder().pen(pen)
+                .program(program)
+                .studentGrade("12")
+                .studentStatus("A")
+                .schoolOfRecord(mincode)
+                .graduationRequestYear("2018")
+                .programCodes(Arrays.asList("ID")).build();
+        ConversionStudentSummaryDTO summary = new ConversionStudentSummaryDTO();
+        summary.setAccessToken("123");
+
+        when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
+
+        ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -412,6 +659,29 @@ public class OngoingUpdateServiceTest {
 
         when(this.studentService.loadStudentData(eq(pen), any())).thenReturn(currentStudent);
 
+        when(this.restUtils.getOptionalProgram(eq(program), eq("XC"), any())).thenReturn(null);
+
+        OptionalProgram optionalProgram1 = new OptionalProgram();
+        optionalProgram1.setOptionalProgramID(UUID.randomUUID());
+        optionalProgram1.setOptProgramCode("FI");
+        optionalProgram1.setOptionalProgramName("French Immersion");
+        optionalProgram1.setGraduationProgramCode(program);
+        when(this.restUtils.getOptionalProgram(eq(program), eq("FI"), any())).thenReturn(optionalProgram1);
+
+        OptionalProgram optionalProgram2 = new OptionalProgram();
+        optionalProgram2.setOptionalProgramID(UUID.randomUUID());
+        optionalProgram2.setOptProgramCode("AD");
+        optionalProgram2.setOptionalProgramName("Advance Placement");
+        optionalProgram2.setGraduationProgramCode(program);
+        when(this.restUtils.getOptionalProgram(eq(program), eq("AD"), any())).thenReturn(optionalProgram2);
+
+        OptionalProgram optionalProgram3 = new OptionalProgram();
+        optionalProgram3.setOptionalProgramID(UUID.randomUUID());
+        optionalProgram3.setOptProgramCode("ID");
+        optionalProgram3.setOptionalProgramName("IB Diploma");
+        optionalProgram3.setGraduationProgramCode(program);
+        when(this.restUtils.getOptionalProgram(eq(program), eq("ID"), any())).thenReturn(optionalProgram3);
+
         // ConvGradStudent = traxStudent with the recent updated info
         ConvGradStudent traxStudent = ConvGradStudent.builder().pen(pen)
                 .program(program)
@@ -424,8 +694,12 @@ public class OngoingUpdateServiceTest {
         summary.setAccessToken("123");
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -491,8 +765,12 @@ public class OngoingUpdateServiceTest {
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
         when(this.studentService.hasAnyFrenchImmersionCourse(any(), any(), any(), any())).thenReturn(true);
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -558,8 +836,12 @@ public class OngoingUpdateServiceTest {
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
         when(this.studentService.hasAnyFrenchImmersionCourse(any(), any(), any(), any())).thenReturn(false);
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
     @Test
@@ -609,8 +891,12 @@ public class OngoingUpdateServiceTest {
         summary.setAccessToken("123");
 
         when(this.restUtils.getTraxStudentMasterDataByPen(eq(pen), any())).thenReturn(Arrays.asList(traxStudent));
+        when(this.eventRepository.findByEventId(event.getEventId())).thenReturn(Optional.of(event));
 
         ongoingUpdateService.processEvent(traxUpdateInGrad, event);
+
+        assertThat(event).isNotNull();
+        assertThat(event.getEventStatus()).isEqualTo(EventStatus.PROCESSED.name());
     }
 
 }
