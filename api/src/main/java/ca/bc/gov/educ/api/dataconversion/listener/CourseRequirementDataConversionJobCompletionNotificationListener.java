d517908d3bd91965f0c351d58509b30c0f1fede1
Jinil Sung <infstar@gmail.com>
2023-03-14 22:58:26 -0700

    job completion listener updates for spring batch 5.
    job completion listener updates for spring batch 5.

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/listener/CourseRequirementDataConversionJobCompletionNotificationListener.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/listener/CourseRequirementDataConversionJobCompletionNotificationListener.java
@@ -19,7 +19,9 @@ public class CourseRequirementDataConversionJobCompletionNotificationListener im
     
     @Override
     public void afterJob(JobExecution jobExecution) {
-    	if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+		if (jobExecution.getStatus() == BatchStatus.COMPLETED ||
+			jobExecution.getStatus() == BatchStatus.FAILED ||
+			jobExecution.getStatus() == BatchStatus.UNKNOWN) {
 			long elapsedTimeMillis = new Date().getTime() - jobExecution.getStartTime().atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
 			LOGGER.info("=======================================================================================");
 	    	LOGGER.info("Data Conversion - Course Requirement Job completed in {} s with jobExecution status {}", elapsedTimeMillis/1000, jobExecution.getStatus().toString());
