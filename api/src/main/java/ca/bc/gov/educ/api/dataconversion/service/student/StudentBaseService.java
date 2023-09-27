26aca9e8e23881d658f713ad1db0a9c8e50d3a74
chris.ditcher <Chris.Ditcher@QP.GOV.BC.CA>
2023-09-27 16:33:51 -0700

    Revert "Removed code smells."
    This reverts commit 9c4db718bd2dfb8ad96118d5566867eaa946048f.

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/service/student/StudentBaseService.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/service/student/StudentBaseService.java
@@ -14,6 +14,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import static ca.bc.gov.educ.api.dataconversion.util.EducGradDataConversionApiConstants.DEFAULT_DATE_FORMAT;
+
 public class StudentBaseService {
 
     // Student Status
