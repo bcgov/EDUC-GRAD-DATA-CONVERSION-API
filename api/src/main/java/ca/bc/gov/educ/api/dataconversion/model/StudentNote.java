40a8e242efa8ebb05fc78cff604adfb655819631
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-03-04 09:38:42 -0700

    GRAD2-2465 - Delete a student and related data

--- api/src/main/java/ca/bc/gov/educ/api/dataconversion/model/StudentNote.java
+++ api/src/main/java/ca/bc/gov/educ/api/dataconversion/model/StudentNote.java
@@ -3,12 +3,10 @@ package ca.bc.gov.educ.api.dataconversion.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
 @Data
-@SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentNote extends BaseModel {
