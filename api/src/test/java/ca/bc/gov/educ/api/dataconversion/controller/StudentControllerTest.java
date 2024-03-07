6932caf1cdae817a826bdf7890ecc8c2fc9b4573
Kamal Mohammed <kamal.mohammed@outlook.com>
2024-03-07 14:13:44 -0700

    GRAD2-2465 - Delete a student and related data

--- api/src/test/java/ca/bc/gov/educ/api/dataconversion/controller/StudentControllerTest.java
+++ api/src/test/java/ca/bc/gov/educ/api/dataconversion/controller/StudentControllerTest.java
@@ -5,18 +5,10 @@ import ca.bc.gov.educ.api.dataconversion.service.student.StudentService;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.batch.core.configuration.JobRegistry;
-import org.springframework.batch.core.launch.JobLauncher;
-
-import java.util.List;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
 @ExtendWith(MockitoExtension.class)
@@ -30,13 +22,11 @@ public class StudentControllerTest {
 
         String pen = "123456789";
         String accessToken = "Bearer accesstoken";
-
         Student student = new Student();
 
         Mockito.when(studentService.getStudentByPen(pen,accessToken.replaceAll("Bearer ", accessToken))).thenReturn(student);
         studentService.getStudentByPen(pen,accessToken.replaceAll("Bearer ", accessToken));
         Mockito.verify(studentService).getStudentByPen(pen, accessToken.replaceAll("Bearer ", accessToken));
-
     }
 
     @Test
@@ -44,7 +34,6 @@ public class StudentControllerTest {
 
         String pen = "123456789";
         String accessToken = "Bearer accesstoken";
-
         Student student = new Student();
 
         Mockito.when(studentService.cascadeDeleteStudent(pen, accessToken.replaceAll("Bearer ", accessToken))).thenReturn(pen);
